04a03f2872ce9c45e14c4c0124e48465e6ca4238
Soul <joshgingold336@gmail.com>
2024-07-06 18:46:39 -0700

    Restructure project to move all code to my-react-app directory

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: "my-react-app",
+  build: {
+    outDir: "../dist",
+  },
   server: {
     port: 3000,
     open: true,
